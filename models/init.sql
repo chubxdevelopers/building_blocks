7b55b915d7d426f10785472b33ad3e4956953478
CHITUKULA ABHISHEK REDDY <cabhishekreddy1410@gmail.com>
2025-10-17 18:22:37 +0530

    tables updated

--- models/init.sql
+++ models/init.sql
@@ -16,14 +16,20 @@ CREATE TABLE features (
 );
 
 CREATE TABLE features_capability (
-  capability_id VARCHAR(100) PRIMARY KEY,
-  features_json JSON
+  capability_id INT AUTO_INCREMENT PRIMARY KEY,
+  features_json JSON,
+  CONSTRAINT chk_valid_json CHECK (JSON_VALID(features_json))
 );
+
 
 CREATE TABLE role_capability (
   id INT AUTO_INCREMENT PRIMARY KEY,
   role VARCHAR(50),
   team VARCHAR(50),
   company VARCHAR(100),
-  capability_id VARCHAR(100)
+  capability_id INT,
+  FOREIGN KEY (capability_id) REFERENCES features_capability(capability_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
+
